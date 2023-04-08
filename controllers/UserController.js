53bc28761b3b8fbcf151d09db61e29e82d47033f
Rogerio Pessanha <rogerioo_pessanha@yahoo.com.br>
2023-04-08 03:00:08 -0300

    att

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,26 +1,41 @@
 
 var User = require("../models/User")
 
-class UserController{
-    async index(req, res){}
+class UserController {
+    async index(req, res) {
+        var users = await User.findAll()
+        res.json(users)
+    }
 
-    async create(req, res){
-        var {email, password, name} = req.body
+    async findUser(req, res) {
+        var id = req.params.id
+        var user = await User.findById(id)
+        if (user == undefined) {
+            res.status(404)
+            res.json()
+        } else {
+            res.status(200)
+            res.json(user)
+        }
+    }
+
+    async create(req, res) {
+        var { email, password, name } = req.body
 
         if (name == undefined) {
             res.status(400)
-            res.json({erro: "Nome incorreto!"})
+            res.json({ erro: "Nome incorreto!" })
             return
         }
         if (password == undefined) {
             res.status(400)
-            res.json({erro: "Sua senha está incorreta ou incompleta!"})
+            res.json({ erro: "Sua senha está incorreta ou incompleta!" })
             return
         }
 
         if (email == undefined) {
             res.status(400)
-            res.json({erro: "O email é inválido!"})
+            res.json({ erro: "O email é inválido!" })
             return
         }
 
@@ -29,7 +44,7 @@ class UserController{
 
         if (emailExists) {
             res.status(406)
-            res.json({erro: "O e-mail já está cadastrado!"})
+            res.json({ erro: "O e-mail já está cadastrado!" })
             return
         }
 
@@ -37,6 +52,24 @@ class UserController{
 
         res.status(200)
         res.send("TUDO OK!")
+    }
+
+    async edit(req, res) {
+        var { id, name, email, role } = req.body
+        var result = await User.update(id, name, email, role)
+
+        if (result != undefined) {
+            if (result.status) {
+                res.status(200)
+                res.send("TUDO OK")
+            } else {
+                res.status(406)
+                res.send(result.erro)
+            }
+        }else{
+            res.status(406)
+                res.send("Ocorreu um erro no servidor!")
+        }
     }
 }
 
--- models/User.js
+++ models/User.js
@@ -3,34 +3,98 @@ var knex = require("../database/connection")
 var bcrypt = require("bcrypt")
 
 class User {
-    async new(email, password, name) {
 
+    //buscar os dados de todos os usuarios no sistema
+    async findAll() {
         try {
+            var result = await knex.select(["id", "name", "email", "role"]).table("users")
+            return result
+        } catch (erro) {
+            console.log(erro)
+            return []
+        }
+    }
 
+    //buscando usuario pelo id
+    async findById(id) {
+        try {
+            var result = await knex.select(["id", "name", "email", "role"]).where({ id: id }).table("users")
+            if (result.length > 0) {
+                return result[0]
+            } else {
+                return undefined
+            }
+        } catch (erro) {
+            console.log(erro)
+            return undefined
+        }
+    }
+
+
+    //usando o bcrypt para a senha
+    async new(email, password, name) {
+        try {
             var hash = await bcrypt.hash(password, 10)
-
             await knex.insert({ email, password: hash, name, role: 0 }).table("users")
         } catch (erro) {
             console.log(erro)
         }
     }
 
-    async findEmail(email){
-        
-        try{
-            var result = await knex.select("*").from("users").where({email: email})
+
+    async findEmail(email) {
+        try {
+            var result = await knex.select("*").from("users").where({ email: email })
 
             if (result.length > 0) {
                 return true
-            }else{
+            } else {
                 return false
             }
 
-        } catch(erro){
+        } catch (erro) {
             console.log(erro)
             return false
         }
     }
+
+    //editando um usuario
+    async update(id, name, email, role) {
+        var user = await this.findById(id)
+        if (user != undefined) {
+            var editUser = {}
+
+            if (email != undefined) {
+                if (email != user.email) {
+                    var result = await this.findEmail(email)
+                    if (result == false) {
+                        editUser.email = email
+                    }else{
+                        return { status: false, erro: "O e-mail já está cadastrado" }
+                    }
+                }
+            }
+
+            if (name != undefined) {
+                editUser.name = name
+            }
+
+            if (role != undefined) {
+                editUser.role = role
+            }
+
+            try{
+                await knex.update(editUser).where({id: id}).table("users")
+                return { status: true }
+            }catch(erro){
+                return { status: false, erro: erro }
+            }
+
+        } else {
+            return { status: false, erro: "O usuario não existe" }
+        }
+    }
+
 }
 
 module.exports = new User()
