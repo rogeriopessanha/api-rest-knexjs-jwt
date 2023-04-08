103ccc7f9914102e2df9a3912990a331a8c39d48
Rogerio Pessanha <rogerioo_pessanha@yahoo.com.br>
2023-04-07 23:54:22 -0300

    att

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -7,18 +7,31 @@ class UserController{
     async create(req, res){
         var {email, password, name} = req.body
 
+        if (name == undefined) {
+            res.status(400)
+            res.json({erro: "Nome incorreto!"})
+            return
+        }
+        if (password == undefined) {
+            res.status(400)
+            res.json({erro: "Sua senha está incorreta ou incompleta!"})
+            return
+        }
+
         if (email == undefined) {
             res.status(400)
             res.json({erro: "O email é inválido!"})
+            return
         }
-        // if (name == undefined) {
-        //     res.status(400)
-        //     res.json({erro: "Nome incorreto!"})
-        // }
-        // if (password == undefined) {
-        //     res.status(400)
-        //     res.json({erro: "Sua senha está incorreta ou incompleta!"})
-        // }
+
+        //verificar se o email já existe no database
+        var emailExists = await User.findEmail(email)
+
+        if (emailExists) {
+            res.status(406)
+            res.json({erro: "O e-mail já está cadastrado!"})
+            return
+        }
 
         await User.new(email, password, name)
 
--- models/User.js
+++ models/User.js
@@ -6,9 +6,29 @@ class User {
     async new(email, password, name) {
 
         try {
-            await knex.insert({ email, password, name, role: 0 }).table("users")
+
+            var hash = await bcrypt.hash(password, 10)
+
+            await knex.insert({ email, password: hash, name, role: 0 }).table("users")
         } catch (erro) {
             console.log(erro)
+        }
+    }
+
+    async findEmail(email){
+        
+        try{
+            var result = await knex.select("*").from("users").where({email: email})
+
+            if (result.length > 0) {
+                return true
+            }else{
+                return false
+            }
+
+        } catch(erro){
+            console.log(erro)
+            return false
         }
     }
 }
