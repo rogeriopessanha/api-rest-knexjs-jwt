d902350413282b6d740b1c07d6d6e45a647e639c
Rogerio Pessanha <rogerioo_pessanha@yahoo.com.br>
2023-04-09 15:43:53 -0300

    att

---
+++ middleware/AdminAuth.js
@@ -0,0 +1,31 @@
+
+var jwt = require("jsonwebtoken")
+var secret = process.env.DB_SECRET;
+module.exports = function (req, res, next) {
+    const authToken = req.headers['authorization']
+
+    if (authToken != undefined) {
+        const bearer = authToken.split(' ')
+        var token = bearer[1]
+        try{
+            var decoded = jwt.verify(token, secret)
+            if (decoded.role == 1) {
+                next()
+            }else{
+                res.status(403)
+        res.send("Você não tem autorização!")
+        return
+            }
+        }catch(erro){
+            res.status(403)
+        res.send("Você não está autenticado")
+        return
+        }
+
+
+    }else{
+        res.status(403)
+        res.send("Você não está autenticado")
+        return
+    }
+}
